ed068403ccc62db9ff275d90e6ab224ed69daf07
Finn Farrer <finnfarrer@yahoo.co.uk>
2025-08-06 00:55:12 +0100

    changes to report.js

--- model/report.js
+++ model/report.js
@@ -26,19 +26,7 @@ function generateOutput( teams, regions = [0,1,2], strDate ){
 
     let fileDate = strDate.replaceAll('-','_');
     let year = fileDate.slice(0,4);
-    let dayOfMonth = Number( strDate.slice(-2) );
-    let invitationFolder = `../invitation/${year}/`;  
-    let liveFolder= `../live/${year}/`
-
-    if ( dayOfMonth < 8 ) {                  
-        if ( !fs.existsSync( invitationFolder + `${ summaryFolder }${ fileDate }/` ) )
-            fs.mkdirSync( ( invitationFolder + `${ summaryFolder }${ fileDate }/`), { recursive: true } );
-        
-        fs.writeFileSync( `${invitationFolder}standings_global_${ fileDate }${ format }`, displayRankings( teams, [0,1,2], strDate ) );
-        fs.writeFileSync( `${invitationFolder}standings_europe_${ fileDate }${ format }`, displayRankings( teams, [0], strDate ) );
-        fs.writeFileSync( `${invitationFolder}standings_americas_${ fileDate }${ format }`, displayRankings( teams, [1], strDate ) );
-        fs.writeFileSync( `${invitationFolder}standings_asia_${ fileDate }${ format }`, displayRankings( teams, [2], strDate ) );
-    }
+    let liveFolder= `../liquipedia/live/${year}/`
 
     if ( !fs.existsSync( liveFolder + `${ summaryFolder }${ fileDate }/` ) )
         fs.mkdirSync( (liveFolder + `${ summaryFolder }${ fileDate }/`), { recursive: true } );
@@ -52,13 +40,9 @@ function generateOutput( teams, regions = [0,1,2], strDate ){
         if (t.globalRank > 0 ){
             let paddedRank = t.globalRank.toString().padStart(4,'0');
 
-            t.filename =  `${ summaryFolder }${ fileDate }/${ paddedRank }--${ sanitize( t.name ) }--${ sanitizeRoster( t.activeRoster ) }${ format }`;
-            
+            t.filename =  `${ summaryFolder }${ fileDate }/${ paddedRank }--${ sanitize( t.name ) }--${ sanitizeRoster( t.players ) }${ format }`;
             fs.writeFileSync( `${ liveFolder }${ t.filename }`, displayTeamRankingSummary( t, teams, strDate ) );
 
-            if ( dayOfMonth < 8 ) {
-                fs.writeFileSync( `${ invitationFolder }${ t.filename }`, displayTeamRankingSummary( t, teams, strDate ) );
-            }
         }
     });    
 }
@@ -114,8 +98,8 @@ function displayRankings( teams, regions = [0,1,2], strDate ) {
             table.addElem( displayRank );
             table.addElem( t.glickoTeam.rank() );
             table.addElem( t.name );
-            table.addElem( sortCaseInsensitive( t.activeRoster.map(p => p.nick) ).join(', ') );
-            table.addElem( `[details](${ summaryFolder }${ fileDate }/${ paddedRank }--${ sanitize( t.name ) }--${ sanitizeRoster( t.activeRoster )}${ format })` );
+            table.addElem( sortCaseInsensitive( t.players.map(p => p.nick) ).join(', ') );
+            table.addElem( `[details](${ summaryFolder }${ fileDate }/${ paddedRank }--${ sanitize( t.name ) }--${ sanitizeRoster( t.players )}${ format })` );
             table.commitRow();
         }
     });
@@ -124,7 +108,7 @@ function displayRankings( teams, regions = [0,1,2], strDate ) {
     output += '\n' + tableString + '\n';
 
     output += formatLine( '', true );
-    output += formatLine( '_Event data for Regional Standings provided by HLTV.org_' );
+    output += formatLine( '_Event data for Regional Standings provided by LiquipediaDB_' );
 
     //console.log( output );
     return output;
@@ -134,7 +118,7 @@ function displayRankings( teams, regions = [0,1,2], strDate ) {
 
 function displayTeamRankingSummary( team, teams, strDate ){
     
-    let roster = sortCaseInsensitive( team.activeRoster.map( el=> el.nick ) ).join(', ');
+    let roster = sortCaseInsensitive( team.players.map( el=> el.nick ) ).join(', ');
     let minSeedValue = Math.min( ...teams.map(t => t.seedValue ) );
     let maxSeedValue = Math.max( ...teams.map(t => t.seedValue ) );
     let fileDate = strDate.replaceAll('-','_');
@@ -298,7 +282,7 @@ function displayTeamRankingSummary( team, teams, strDate ){
     output += formatLine( `<span id="curveFunction"></span>_The Curve Function: 1 / ( 1 + abs( log10( x ) ) )_` );
     output += formatLine( '', true );
     output += formatLine( '---', true );
-    output += formatLine( '_Event data for Regional Standings provided by HLTV.org_' );
+    output += formatLine( '_Event data for Regional Standings provided by LiquipediaDB_' );
 
     return output;
 }
